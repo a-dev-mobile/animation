cff8f42a090e876f1b3dd260aac689c21a533d3d
Dmitriy Trofimov <dmitry.trofimov@aventus.work>
2022-04-24 13:07:26 +0300

    feat: adding new project

---
+++ android/app/src/main/kotlin/a/dev/mobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package a.dev.mobile.animation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,13 @@
+// Copyright (c) 2022, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+import 'package:animation/app/app.dart';
+import 'package:animation/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
